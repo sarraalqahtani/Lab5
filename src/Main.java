908c90d45484ca8e18b6478ce6ed443d6a05d8f4
Alqahtani <sarra-alqahtani@wfu.edu>
2020-10-22 10:03:49 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class Main {
+
+    int binarySearch(LinkedList a, Iris item, int low, int high) {
+        return 0;
+    }
+
+    void insertionSort(LinkedList a, int n) {
+
+    }
+
+        public static void main(String [] args){
+    }
+}
